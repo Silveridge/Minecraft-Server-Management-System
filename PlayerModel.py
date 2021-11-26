46a2a9384fba789f052662cc6da5cb98d3feaf93
Silveridge <sam.fitzpatrick2003@gmail.com>
2021-11-27 01:56:59 +1100

    Major 0.1 Update
    - Added auto UUID to Name conversion functionality from playerdata file
    - Autosorts only lines involving each player into separate files
    - Identifies different features (advancements, names, total playtime) involving each player
    - Stores data in a player class object

---
+++ PlayerModel.py
@@ -0,0 +1,9 @@
+class PlayerModel():
+    def __init__(self):
+        self.name = ""
+        self.totalPlaytime = 0
+        self.serverOperator = False
+        self.advancementsMade = 0
+        self.advancements = []
+        self.uuid = ""
+        self.ips = []
