ce729b43ed224173dc07d5bc68ffea1d74fc913c
Silveridge <sam.fitzpatrick2003@gmail.com>
2021-11-30 22:36:19 +1100

    GUI Incorporation
    Added temporary gui functionality
    Supported new server additions (includes most recent server location, server location history and the ability to reformat new additions without breaking it)

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
 from PlayerStats.findPlayerStats import *
 from ServerInfo.getServerInfo import *
+import gui
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 
-path = input("Path to server file: ")
-m = Main(path)
-#s = getServerInfo(path)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Widget = QtWidgets.QWidget()
+    ui = gui.Ui_Widget()
+    ui.setupUi(Widget)
+
+    if len(open("cache/serverLocations.txt").readlines()) <= 0:
+        noPreviousLocation = True
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("You do not appear to have any previous server locations selected.")
+        msg.setInformativeText("No worries! We'll popup a box to let you select one. Simply select the server directory ONLY!")
+        msg.setWindowTitle("No previous locations found!")
+        msg.exec_()
+
+        while noPreviousLocation:
+            location = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
+            if location != "":
+                noPreviousLocation = False
+                with open("cache/serverLocations.txt","a") as file:
+                    file.write(str(location) + "\n")
+                    file.close()
+                
+                with open("cache/mostRecentServer.txt","a") as file:
+                    file.write(str(location) + "\n")
+                    file.close()
+
+    elif len(open("cache/mostRecentServer.txt").readlines()) <= 0:
+        location = open("cache/serverLocations.txt").readlines()[0].strip("\n")
+        with open("cache/mostRecentServer.txt","a") as file:
+                    file.write(str(location) + "\n")
+                    file.close()
+
+        
+    
+    ## Create start.bat file
+    batFile = open(f"{location}\start.bat","w")
+    batFile.write("java -Xmx4G -Xms2G -jar forge-1.16.5-36.2.0.jar nogui")
+    batFile.close()
+
+    Widget.show()
+
+    sys.exit(app.exec_())
+
+
+#path = input("Path to server file: ")
